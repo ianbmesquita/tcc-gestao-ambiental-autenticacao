68b7d008ae8657c29484b15308b4913e92f680c2
Ian Mesquita <ianmesquitadb@gmail.com>
2023-09-24 02:13:59 -0300

    feat: Criando estrutura de Exception Handling do módulo.

--- src/main/java/br/pucmg/sigam/autenticacao/infra/configurations/CorsConfiguration.java
+++ src/main/java/br/pucmg/sigam/autenticacao/infra/configurations/CorsConfiguration.java
@@ -14,8 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://pxk2kufc4a.execute-api.sa-east-1.amazonaws.com")
-//                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
                         .allowCredentials(true);
             }
